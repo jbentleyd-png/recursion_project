f886537340f0bfd8a78c627bbb9870eb1128007c
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-12 14:46:07 -0600

    solved merge_sort
    It's all in the comparison signs.

--- merge_try_2.rb
+++ merge_try_2.rb
@@ -1,17 +1,18 @@
 def merge_sort(arr)
+  # edge cases:
   return arr if arr.length <= 1
-  # cut it in half
 
+  # cut it in half:
   left_arr = arr[0...arr.length / 2]; right_arr = arr[arr.length / 2...arr.length]
-  p left_arr
-  p right_arr
-  output = []
 
-  if right_arr.length == 2
+  # recurse:
+  if right_arr.length >= 2
     right_arr =  merge_sort(right_arr)
     left_arr = merge_sort(left_arr)
   end
-
+  
+  # sort:
+  output = []
   until left_arr.empty? || right_arr.empty?
     left_arr[0] < right_arr[0] ? output << left_arr.delete_at(0) : output << right_arr.delete_at(0)
   end
@@ -19,9 +20,12 @@ def merge_sort(arr)
   output
 end
 
+
+
 # p merge_sort([])
 # p merge_sort([73])
 # p merge_sort([2, 1])
 # p merge_sort([3, 2, 1])
 p merge_sort([4, 3, 2, 1])
 p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([105, 79, 100, 110])
