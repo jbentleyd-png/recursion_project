058682c149fa27a79bf71e428a094031fd71dbf7
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-12 09:51:13 -0600

    fib iterative works for first two values

---
+++ fibonacci.rb
@@ -0,0 +1,15 @@
+# iterated version:
+
+def fibs_it(num)
+  return nil unless num.is_a?(Integer) && num > 0
+  initial_array = [0,1]
+  
+  if num > 2
+    
+  end
+
+  return initial_array[0...num]
+end
+
+p fibs_it(1)
+p fibs_it(2)
