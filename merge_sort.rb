7c8c4762196b5f87164ac20725d84b183faf0e9a
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-12 11:35:25 -0600

    achieved recursive splitting

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,10 +1,20 @@
 def merge_sort(arr)
   # cut it in half
-  left_arr = arr[0...arr.length / 2]
-  right_arr = arr[arr.length / 2...arr.length]
-  p left_arr
-  p right_arr
+
+  left_arr = arr[0...arr.length / 2]; right_arr = arr[arr.length / 2...arr.length]
+  if left_arr == []
+    p right_arr
+    return right_arr
+  end
+
+  macro_arr = []; macro_arr << left_arr; macro_arr << right_arr
+  p macro_arr
   # sort each half
+  macro_arr.each do |a|
+    break if a.empty?
+    merge_sort(a)
+  end
+
   # merge
   # recurse?
 end
