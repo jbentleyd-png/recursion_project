d0e1079bc5bb62a75166ed30d6a68979f4e78503
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-12 13:57:36 -0600

    Can sort two numbers/halves of an array.

--- merge_try_2.rb
+++ merge_try_2.rb
@@ -1,8 +1,17 @@
 def merge_sort(arr)
+  return arr if arr.length <= 1
   # cut it in half
 
   left_arr = arr[0...arr.length / 2]; right_arr = arr[arr.length / 2...arr.length]
-
-end`
+  output = []
+  until left_arr.empty? || right_arr.empty?
+    left_arr[0] < right_arr[0] ? output << left_arr.delete_at(0) : output << right_arr.delete_at(0)
+  end
+    left_arr.empty? ? output += right_arr : output += left_arr
+  output
+end
 
 p merge_sort([])
+p merge_sort([73])
+p merge_sort([2, 1])
+p merge_sort([3, 2, 1])
