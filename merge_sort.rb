f3b1fefb1def4d124995a25706ea276298b898d1
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-12 11:01:12 -0600

    merge_sort splits array

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,6 +1,12 @@
 def merge_sort(arr)
   # cut it in half
+  left_arr = arr[0...arr.length / 2]
+  right_arr = arr[arr.length / 2...arr.length]
+  p left_arr
+  p right_arr
   # sort each half
   # merge
   # recurse?
 end
+
+merge_sort([5,2,1,3,6,4])
